02a8f13c5d9bb2df81f7a125c9e1df484d984cd5
Farès MOULOUDI <fares.mld@icloud.com>
2025-03-12 09:25:26 +0100

    Mise à jour rôles et users

--- roles.sql
+++ roles.sql
@@ -5,58 +5,58 @@ ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
 ------------------------------------------
 
 ----------------------------
---  Rôle Observateur
+--  Rôle Superviseur
 ----------------------------
-CREATE ROLE CYPI_CERGY_OBSERVATEUR;
+CREATE ROLE CYPI_CERGY_SUPERVISEUR;
 
 --  Accorder l'accès en lecture (SELECT) à toutes les vues
 DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR v IN (SELECT view_name FROM all_views WHERE owner = 'CYPI_CERGY') LOOP
-    v_sql := 'GRANT SELECT ON CYPI_CERGY.' || v.view_name || ' TO CYPI_CERGY_OBSERVATEUR';
+    v_sql := 'GRANT SELECT ON CYPI_CERGY.' || v.view_name || ' TO CYPI_CERGY_SUPERVISEUR';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
 /
 
-GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_OBSERVATEUR;
+GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_SUPERVISEUR;
 
 ----------------------------
---  Rôle Analyste
+--  Rôle INGENIEUR
 ----------------------------
-CREATE ROLE CYPI_CERGY_ANALYSTE;
-GRANT CYPI_CERGY_OBSERVATEUR TO CYPI_CERGY_ANALYSTE;
+CREATE ROLE CYPI_CERGY_INGENIEUR;
+GRANT CYPI_CERGY_SUPERVISEUR TO CYPI_CERGY_INGENIEUR;
 
 --  Accorder l'accès en lecture (SELECT) sur toutes les tables
 DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR tbl IN (SELECT table_name FROM all_tables WHERE owner = 'CYPI_CERGY') LOOP
-    v_sql := 'GRANT SELECT ON CYPI_CERGY.' || tbl.table_name || ' TO CYPI_CERGY_ANALYSTE';
+    v_sql := 'GRANT SELECT ON CYPI_CERGY.' || tbl.table_name || ' TO CYPI_CERGY_INGENIEUR';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
 /
 
 --  Autoriser la modification des tickets et commentaires
-GRANT UPDATE ON CYPI_CERGY.TICKETS TO CYPI_CERGY_ANALYSTE;
-GRANT UPDATE ON CYPI_CERGY.COMMENTAIRES_TICKETS TO CYPI_CERGY_ANALYSTE;
+GRANT UPDATE ON CYPI_CERGY.TICKETS TO CYPI_CERGY_INGENIEUR;
+GRANT UPDATE ON CYPI_CERGY.COMMENTAIRES_TICKETS TO CYPI_CERGY_INGENIEUR;
 
-GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_ANALYSTE;
+GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_INGENIEUR;
 
 ----------------------------
---  Rôle Développeur
+--  Rôle EXPERT
 ----------------------------
-CREATE ROLE CYPI_CERGY_DEV;
-GRANT CYPI_CERGY_ANALYSTE TO CYPI_CERGY_DEV;
+CREATE ROLE CYPI_CERGY_EXPERT;
+GRANT CYPI_CERGY_INGENIEUR TO CYPI_CERGY_EXPERT;
 
 --  Accorder tous les droits (SELECT, INSERT, UPDATE, DELETE) sur toutes les tables
 DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR tbl IN (SELECT table_name FROM all_tables WHERE owner = 'CYPI_CERGY') LOOP
-    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_CERGY.' || tbl.table_name || ' TO CYPI_CERGY_DEV';
+    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_CERGY.' || tbl.table_name || ' TO CYPI_CERGY_EXPERT';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
@@ -67,7 +67,7 @@ DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR v IN (SELECT view_name FROM all_views WHERE owner = 'CYPI_CERGY') LOOP
-    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_CERGY.' || v.view_name || ' TO CYPI_CERGY_DEV';
+    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_CERGY.' || v.view_name || ' TO CYPI_CERGY_EXPERT';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
@@ -78,84 +78,84 @@ DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR v IN (SELECT sequence_name FROM all_sequences WHERE sequence_owner = 'CYPI_CERGY') LOOP
-    v_sql := 'GRANT SELECT, ALTER ON CYPI_CERGY.' || v.sequence_name || ' TO CYPI_CERGY_DEV';
+    v_sql := 'GRANT SELECT, ALTER ON CYPI_CERGY.' || v.sequence_name || ' TO CYPI_CERGY_EXPERT';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
 /
 
-GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_DEV;
+GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_EXPERT;
 
 ----------------------------
---  Rôle Administrateur
+--  Rôle RESPONSABLE
 ----------------------------
-CREATE ROLE CYPI_CERGY_ADMIN;
+CREATE ROLE CYPI_CERGY_RESPONSABLE;
 
---  Donner tous les privilèges du développeur à l'administrateur
-GRANT CYPI_CERGY_DEV TO CYPI_CERGY_ADMIN;
+--  Donner tous les privilèges de l'expert au responsable
+GRANT CYPI_CERGY_EXPERT TO CYPI_CERGY_RESPONSABLE;
 
 --  Autoriser la création de liens de base de données
-GRANT CREATE DATABASE LINK TO CYPI_CERGY_ADMIN;
+GRANT CREATE DATABASE LINK TO CYPI_CERGY_RESPONSABLE;
 
-GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_ADMIN;
+GRANT CONNECT, CREATE SESSION TO CYPI_CERGY_RESPONSABLE;
 
 ------------------------------------------
 --  Création des rôles pour CYPI_PAU
 ------------------------------------------
 
 ----------------------------
---  Rôle Observateur
+--  Rôle SUPERVISEUR
 ----------------------------
-CREATE ROLE CYPI_PAU_OBSERVATEUR;
+CREATE ROLE CYPI_PAU_SUPERVISEUR;
 
 --  Accorder l'accès en lecture (SELECT) sur toutes les vues
 DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR v IN (SELECT view_name FROM all_views WHERE owner = 'CYPI_PAU') LOOP
-    v_sql := 'GRANT SELECT ON CYPI_PAU.' || v.view_name || ' TO CYPI_PAU_OBSERVATEUR';
+    v_sql := 'GRANT SELECT ON CYPI_PAU.' || v.view_name || ' TO CYPI_PAU_SUPERVISEUR';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
 /
 
-GRANT CONNECT, CREATE SESSION TO CYPI_PAU_OBSERVATEUR;
+GRANT CONNECT, CREATE SESSION TO CYPI_PAU_SUPERVISEUR;
 
 ----------------------------
---  Rôle Analyste
+--  Rôle INGENIEUR
 ----------------------------
-CREATE ROLE CYPI_PAU_ANALYSTE;
-GRANT CYPI_PAU_OBSERVATEUR TO CYPI_PAU_ANALYSTE;
+CREATE ROLE CYPI_PAU_INGENIEUR;
+GRANT CYPI_PAU_SUPERVISEUR TO CYPI_PAU_INGENIEUR;
 
 --  Accorder l'accès en lecture (SELECT) sur toutes les tables
 DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR tbl IN (SELECT table_name FROM all_tables WHERE owner = 'CYPI_PAU') LOOP
-    v_sql := 'GRANT SELECT ON CYPI_PAU.' || tbl.table_name || ' TO CYPI_PAU_ANALYSTE';
+    v_sql := 'GRANT SELECT ON CYPI_PAU.' || tbl.table_name || ' TO CYPI_PAU_INGENIEUR';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
 /
 
 --  Autoriser la modification des tickets et commentaires
-GRANT UPDATE ON CYPI_PAU.TICKETS TO CYPI_PAU_ANALYSTE;
-GRANT UPDATE ON CYPI_PAU.COMMENTAIRES_TICKETS TO CYPI_PAU_ANALYSTE;
+GRANT UPDATE ON CYPI_PAU.TICKETS TO CYPI_PAU_INGENIEUR;
+GRANT UPDATE ON CYPI_PAU.COMMENTAIRES_TICKETS TO CYPI_PAU_INGENIEUR;
 
-GRANT CONNECT, CREATE SESSION TO CYPI_PAU_ANALYSTE;
+GRANT CONNECT, CREATE SESSION TO CYPI_PAU_INGENIEUR;
 
 ----------------------------
---  Rôle Développeur
+--  Rôle EXPERT
 ----------------------------
-CREATE ROLE CYPI_PAU_DEV;
-GRANT CYPI_PAU_ANALYSTE TO CYPI_PAU_DEV;
+CREATE ROLE CYPI_PAU_EXPERT;
+GRANT CYPI_PAU_INGENIEUR TO CYPI_PAU_EXPERT;
 
 --  Accorder tous les droits (SELECT, INSERT, UPDATE, DELETE) sur toutes les tables
 DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR tbl IN (SELECT table_name FROM all_tables WHERE owner = 'CYPI_PAU') LOOP
-    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_PAU.' || tbl.table_name || ' TO CYPI_PAU_DEV';
+    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_PAU.' || tbl.table_name || ' TO CYPI_PAU_EXPERT';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
@@ -166,7 +166,7 @@ DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR v IN (SELECT view_name FROM all_views WHERE owner = 'CYPI_PAU') LOOP
-    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_PAU.' || v.view_name || ' TO CYPI_PAU_DEV';
+    v_sql := 'GRANT SELECT, INSERT, UPDATE, DELETE ON CYPI_PAU.' || v.view_name || ' TO CYPI_PAU_EXPERT';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
@@ -177,35 +177,35 @@ DECLARE
   v_sql VARCHAR2(2000);
 BEGIN
   FOR v IN (SELECT sequence_name FROM all_sequences WHERE sequence_owner = 'CYPI_PAU') LOOP
-    v_sql := 'GRANT SELECT, ALTER ON CYPI_PAU.' || v.sequence_name || ' TO CYPI_PAU_DEV';
+    v_sql := 'GRANT SELECT, ALTER ON CYPI_PAU.' || v.sequence_name || ' TO CYPI_PAU_EXPERT';
     EXECUTE IMMEDIATE v_sql;
   END LOOP;
 END;
 /
 
-GRANT CONNECT, CREATE SESSION TO CYPI_PAU_DEV;
+GRANT CONNECT, CREATE SESSION TO CYPI_PAU_EXPERT;
 
 ----------------------------
--- Rôle Administrateur
+-- Rôle RESPONSABLE
 ----------------------------
-CREATE ROLE CYPI_PAU_ADMIN;
+CREATE ROLE CYPI_PAU_RESPONSABLE;
 
---  Donner tous les privilèges du développeur à l'administrateur
-GRANT CYPI_PAU_DEV TO CYPI_PAU_ADMIN;
+--  Donner tous les privilèges du développeur au responsable
+GRANT CYPI_PAU_EXPERT TO CYPI_PAU_RESPONSABLE;
 
 --  Autoriser la création de liens de base de données
-GRANT CREATE DATABASE LINK TO CYPI_PAU_ADMIN;
+GRANT CREATE DATABASE LINK TO CYPI_PAU_RESPONSABLE;
 
-GRANT CONNECT, CREATE SESSION TO CYPI_PAU_ADMIN;
+GRANT CONNECT, CREATE SESSION TO CYPI_PAU_RESPONSABLE;
 
 ----------------------------
---  Rôle Super Admin
+--  Rôle Master
 ----------------------------
-CREATE ROLE CYPI_FULL_ADMIN;
+CREATE ROLE CYPI_MASTER;
 
-GRANT CYPI_CERGY_ADMIN TO CYPI_FULL_ADMIN;
-GRANT CYPI_PAU_ADMIN TO CYPI_FULL_ADMIN;
-GRANT CONNECT, CREATE SESSION TO CYPI_FULL_ADMIN;
+GRANT CYPI_CERGY_RESPONSABLE TO CYPI_MASTER;
+GRANT CYPI_PAU_RESPONSABLE TO CYPI_MASTER;
+GRANT CONNECT, CREATE SESSION TO CYPI_MASTER;
 
 COMMIT;
 
